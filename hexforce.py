8f1f36b8084d2e32dcfadca901dc6bba3d0b53f9
Larissa Kim <105753750+LarissaNamu@users.noreply.github.com>
2024-12-16 16:18:35 -0600

    now has all the champ information in home page!

--- hexforce.py
+++ hexforce.py
@@ -1,14 +1,27 @@
-from flask import Flask
+from api.cdragon import fetch_champs
+from utils.data_parser import parse_champs
+from utils.tft_objects import Champions, Teams
+from flask import Flask, render_template
 app = Flask(__name__)
+
+raw_data = fetch_champs()
+
+champions_list = []     # after for loop ends, this array contains all of the tft's set's champs in champion form
+if raw_data:
+    champs = parse_champs(raw_data)
+    for c in champs:
+        # create champ object and append to champion list
+        temp_champ = Champions(c['name'], c['cost'], c['traits'], c['image_path'])
+        champions_list.append(temp_champ)
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Home Page</h1>"
+    return render_template('home.html', champions = champions_list)
 
 @app.route("/about")
 def about():
-    return "<h1>About Page</h1>"
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
