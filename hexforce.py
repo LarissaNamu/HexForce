ee6b4d35b9503fe7390e2ca3c4f0756dfa7b044a
Larissa Kim <105753750+LarissaNamu@users.noreply.github.com>
2024-12-31 16:36:33 -0600

    form validation for registration made

--- hexforce.py
+++ hexforce.py
@@ -1,7 +1,7 @@
 from api.cdragon import fetch_champs
 from utils.data_parser import parse_champs
 from utils.tft_objects import Champions, Teams
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
 
@@ -28,9 +28,12 @@ def home():
 def about():
     return render_template('about.html', title = "About")
 
-@app.route("/register")
+@app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
 
 @app.route("/login")
